f7edac58c6d8d99b17046f943f599d8805007375
kanis <kkppyy13@gmail.com>
2026-08-20 21:02:14 +0530

    Fix Clerk middleware configuration

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,18 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware(
+  (auth, req) => {
+    // Middleware is intentionally not protecting any routes yet.
+  },
+  {
+    debug: true,
+  }
+);
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and static files
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpg|jpeg|png|gif|svg|ico|woff2?|ttf|map)).*)",
-
-    // Always run for API routes
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     "/(api|trpc)(.*)",
+    "/__clerk/(.*)",
   ],
 };
